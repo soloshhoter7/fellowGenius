0c68cd3cd700960bc32a320b39b19f232205ce98
soloshhoter7 <shubhintee@gmail.com>
2020-08-30 06:54:43 +0530

    http 30-08-2020 - shubham

--- fellowGenius-angular/src/app/service/basic-auth-http-interceptor.service.ts
+++ fellowGenius-angular/src/app/service/basic-auth-http-interceptor.service.ts
@@ -1,56 +1,52 @@
-import { Injectable } from "@angular/core";
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-} from "@angular/common/http";
-import { CookieService } from "ngx-cookie-service";
-import { stringify } from "querystring";
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { stringify } from 'querystring';
 
 @Injectable({
-  providedIn: "root",
+	providedIn: 'root'
 })
 export class BasicAuthHttpInterceptorService implements HttpInterceptor {
-  constructor(private cookieService: CookieService) {}
-  invalidUrls: string[] = [
-    "https://backend.fellowgenius.com/fellowGenius/meeting/sendEmail",
-    "https://backend.fellowgenius.com/fellowGenius/registerUser",
-    "https://backend.fellowgenius.com/fellowGenius/registerSocialLogin",
-    "https://backend.fellowgenius.com/authenticate",
+	constructor(private cookieService: CookieService) {}
+	invalidUrls: string[] = [
+		// "https://backend.fellowgenius.com/fellowGenius/meeting/sendEmail",
+		// "https://backend.fellowgenius.com/fellowGenius/registerUser",
+		// "https://backend.fellowgenius.com/fellowGenius/registerSocialLogin",
+		// "https://backend.fellowgenius.com/authenticate",
 
-    // "http://localhost:5000/fellowGenius/meeting/sendEmail",
-    // // 'http://localhost:5000/fellowGenius/registerStudent',
-    // // 'http://localhost:5000/fellowGenius/registerTutor',
-    // "http://localhost:5000/fellowGenius/registerUser",
-    // "http://localhost:5000/fellowGenius/registerSocialLogin",
-    // // 'http://localhost:5000/authenticateStudent',
-    // // 'http://localhost:5000/authenticateTutor',
-    // "http://localhost:5000/authenticate",
-  ];
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    var token = this.cookieService.get("token");
-    // console.log('interceptor  called !');
-    // console.log(req.url);
-    // if (sessionStorage.getItem('username') && sessionStorage.getItem('token')) {
-    if (this.isUrlValid(req.url)) {
-      if (token != "") {
-        req = req.clone({
-          setHeaders: {
-            Authorization: "Bearer " + token,
-          },
-        });
-      }
-      return next.handle(req);
-    } else {
-      return next.handle(req);
-    }
-  }
+		'http://localhost:8080/fellowGenius/meeting/sendEmail',
+		// 'http://localhost:5000/fellowGenius/registerStudent',
+		// 'http://localhost:5000/fellowGenius/registerTutor',
+		'http://localhost:8080/fellowGenius/registerUser',
+		'http://localhost:8080/fellowGenius/registerSocialLogin',
+		// 'http://localhost:5000/authenticateStudent',
+		// 'http://localhost:5000/authenticateTutor',
+		'http://localhost:8080/authenticate'
+	];
+	intercept(req: HttpRequest<any>, next: HttpHandler) {
+		var token = this.cookieService.get('token');
+		// console.log('interceptor  called !');
+		// console.log(req.url);
+		// if (sessionStorage.getItem('username') && sessionStorage.getItem('token')) {
+		if (this.isUrlValid(req.url)) {
+			if (token != '') {
+				req = req.clone({
+					setHeaders: {
+						Authorization: 'Bearer ' + token
+					}
+				});
+			}
+			return next.handle(req);
+		} else {
+			return next.handle(req);
+		}
+	}
 
-  isUrlValid(url: string) {
-    if (this.invalidUrls.includes(url)) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+	isUrlValid(url: string) {
+		if (this.invalidUrls.includes(url)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
