f865fdc0715917d775b6943bcc790ea7d9b0c3b2
sundareshln <sundareshln@gmail.com>
2020-10-03 12:13:47 +0530

    UI Fix - code pusehd

--- fellowGenius-angular/src/app/search-results/filters-dialog/filters-dialog.component.ts
+++ fellowGenius-angular/src/app/search-results/filters-dialog/filters-dialog.component.ts
@@ -20,7 +20,11 @@ export class FiltersDialogComponent implements OnInit {
     this.ratingFilterApplied = data.ratingFiltersApplied;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
   closeDialog() {
     this.dialogRef.close();
   }
