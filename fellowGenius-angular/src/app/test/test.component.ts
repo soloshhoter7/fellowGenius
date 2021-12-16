4285e6b12cf577d150952b1d43f3e1a8ae108221
soloshhoter7 <shubhintee@gmail.com>
2021-12-16 23:43:10 +0530

    voice -call refatoring - shubham

--- fellowGenius-angular/src/app/test/test.component.ts
+++ fellowGenius-angular/src/app/test/test.component.ts
@@ -52,191 +52,180 @@ declare const window: any;
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css'],
-  
 })
 export class TestComponent implements OnInit {
-  
   channelName: string = 'FG@123456';
   appId: string = '45f3ee50e0fd491aa46bd17c05fc7073';
-  uid= '12345';
-  
+  uid = '12345';
+  clientJoined: boolean = false;
   // create Agora client
-client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
-localTracks = {
-  videoTrack: null,
-  audioTrack: null
-};
-remoteUsers = {};
-// Agora client options
-options = {
-  appid: '45f3ee50e0fd491aa46bd17c05fc7073',
-  channel: 'FG@123456',
-  uid: null,
-  token: null
-};
+  client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+  localTracks = {
+    videoTrack: null,
+    audioTrack: null,
+  };
+  remoteUsers = {};
+  // Agora client options
+  options = {
+    appid: '45f3ee50e0fd491aa46bd17c05fc7073',
+    channel: 'FG@123456',
+    uid: null,
+    token: null,
+  };
 
-mics = []; // all microphones devices you can use
-cams = []; // all cameras devices you can use
-currentMic; // the microphone you are using
-currentCam; // the camera you are using
+  mics = []; // all microphones devices you can use
+  cams = []; // all cameras devices you can use
+  currentMic; // the microphone you are using
+  currentCam; // the camera you are using
 
-volumeAnimation;
-    
+  volumeAnimation;
+  localTrackAudioLevel = '0';
   ngOnInit(): void {
-    //throw new Error('Method not implemented.');
+    console.log('options :', this.options);
+    $('#media-device-test').modal('show');
+    var urlParams = new URL(location.href).searchParams;
+    this.options.appid = this.appId;
+    this.options.channel = this.channelName;
+    this.options.token = urlParams.get('token');
+    this.options.uid = urlParams.get('uid');
+    this.mediaDeviceTest();
+    setInterval(() => {
+      this.localTrackAudioLevel = (
+        this.localTracks.audioTrack.getVolumeLevel() * 100
+      ).toFixed(2);
+    }, 100);
+    // this.volumeAnimation = requestAnimationFrame(this.setVolumeWave);
+  }
+
+  async formJoinSubmit(e) {
     console.log(this.options);
-    
-    // the demo can auto join channel with params in url
-$(async () => {
-  $("#media-device-test").modal("show");
-  $(".cam-list").delegate("a", "click", function(e){
-    this.switchCamera(this.text);
-  });
-  $(".mic-list").delegate("a", "click", function(e){
-    this.switchMicrophone(this.text);
-  });
-
-  var urlParams = new URL(location.href).searchParams;
-  this.options.appid = this.appId;
-  this.options.channel = this.channelName;
-  this.options.token = urlParams.get("token");
-  this.options.uid = urlParams.get("uid");
-  await this.mediaDeviceTest();
-  this.volumeAnimation = requestAnimationFrame(this.setVolumeWave);
-})
-
-
-
-$("#leave").click(function (e) {
-  this.leave();
-})
-
-$("#media-device-test").on("hidden.bs.modal", function (e) {
-  cancelAnimationFrame(this.volumeAnimation);
-  console.log(this.options);
-  if (this.options.appid && this.options.channel) {
-    $("#appid").val(this.options.appid);
-    $("#token").val(this.options.token);
-    $("#channel").val(this.options.channel);
-    $("#join-form").submit();
-  }
-})
-  }
-
-  async formJoinSubmit(e){
     e.preventDefault();
-    $("#join").attr("disabled", true);
-    $("#device-wrapper").css("display", "flex");
+    $('#join').attr('disabled', true);
+    $('#device-wrapper').css('display', 'flex');
     try {
       this.options.appid = this.appId;
-      this.options.token = $("#token").val();
+      this.options.token = $('#token').val();
       this.options.channel = this.channelName;
-      this.options.uid = Number($("#uid").val());
+      this.options.uid = Number($('#uid').val());
       await this.join();
-      if(this.options.token) {
-        $("#success-alert-with-token").css("display", "block");
+      if (this.options.token) {
+        $('#success-alert-with-token').css('display', 'block');
       } else {
-        $("#success-alert a").attr("href", `index.html?appid=${this.options.appid}&channel=${this.options.channel}&token=${this.options.token}`);
-        $("#success-alert").css("display", "block");
+        $('#success-alert a').attr(
+          'href',
+          `index.html?appid=${this.options.appid}&channel=${this.options.channel}&token=${this.options.token}`
+        );
+        $('#success-alert').css('display', 'block');
       }
     } catch (error) {
       console.error(error);
     } finally {
-      $("#leave").attr("disabled", false);
+      $('#leave').attr('disabled', false);
     }
   }
-  
+
   async join() {
     // add event listener to play remote tracks when remote user publishs.
-    console.log("Inside the join method");
-    this.client.on("user-published", this.handleUserPublished);
-    this.client.on("user-unpublished", this.handleUserUnpublished);
-    
+    console.log('Inside the join method');
+    this.client.on('user-published', this.handleUserPublished);
+    this.client.on('user-unpublished', this.handleUserUnpublished);
+
     // join a channel.
-    this.options.uid = 
-    await this.client.join(this.options.appid, this.options.channel, 
-      this.options.token || null, this.options.uid || null);
-  
+    this.options.uid = await this.client.join(
+      this.options.appid,
+      this.options.channel,
+      this.options.token || null,
+      this.options.uid || null
+    );
+
     if (!this.localTracks.audioTrack || !this.localTracks.videoTrack) {
-      [ this.localTracks.audioTrack, this.localTracks.videoTrack ] = await Promise.all([
-        // create local tracks, using microphone and camera
-        AgoraRTC.createMicrophoneAudioTrack({ microphoneId: this.currentMic.deviceId }),
-        AgoraRTC.createCameraVideoTrack({ cameraId: this.currentCam.deviceId })
-      ]);
+      [this.localTracks.audioTrack, this.localTracks.videoTrack] =
+        await Promise.all([
+          // create local tracks, using microphone and camera
+          AgoraRTC.createMicrophoneAudioTrack({
+            microphoneId: this.currentMic.deviceId,
+          }),
+          AgoraRTC.createCameraVideoTrack({
+            cameraId: this.currentCam.deviceId,
+          }),
+        ]);
     }
-  
+
     // play local video track
-    this.localTracks.videoTrack.play("local-player");
-    $("#local-player-name").text(`localVideo(${this.options.uid})`);
-  
+    this.localTracks.videoTrack.play('local-player');
+    $('#local-player-name').text(`localVideo(${this.options.uid})`);
+
     // publish local tracks to channel
     await this.client.publish(Object.values(this.localTracks));
-    console.log("publish success");
+    console.log('publish success');
   }
-  
+
   async mediaDeviceTest() {
     // create local tracks
-    [ this.localTracks.audioTrack, this.localTracks.videoTrack ] = await Promise.all([
-      // create local tracks, using microphone and camera
-      AgoraRTC.createMicrophoneAudioTrack(),
-      AgoraRTC.createCameraVideoTrack()
-    ]);
-  
+    [this.localTracks.audioTrack, this.localTracks.videoTrack] =
+      await Promise.all([
+        // create local tracks, using microphone and camera
+        AgoraRTC.createMicrophoneAudioTrack(),
+        AgoraRTC.createCameraVideoTrack(),
+      ]);
+
     // play local track on device detect dialog
-    this.localTracks.videoTrack.play("pre-local-player");
+    this.localTracks.videoTrack.play('pre-local-player');
     // localTracks.audioTrack.play();
-  
+
     // get mics
     this.mics = await AgoraRTC.getMicrophones();
     this.currentMic = this.mics[0];
-    $(".mic-input").val(this.currentMic.label);
-    this.mics.forEach(mic => {
-      $(".mic-list").append(`<a class="dropdown-item" href="#">${mic.label}</a>`);
-    });
-  
+    $('.mic-input').val(this.currentMic.label);
+    // this.mics.forEach((mic) => {
+    //   $('.mic-list').append(
+    //     `<a class="dropdown-item" href="#">${mic.label}</a>`
+    //   );
+    // });
+
     // get cameras
     this.cams = await AgoraRTC.getCameras();
     this.currentCam = this.cams[0];
-    $(".cam-input").val(this.currentCam.label);
-    this.cams.forEach(cam => {
-      $(".cam-list").append(`<a class="dropdown-item" href="#">${cam.label}</a>`);
-    });
+    $('.cam-input').val(this.currentCam.label);
+    // this.cams.forEach((cam) => {
+    //   $('.cam-list').append(
+    //     `<a class="dropdown-item" (click)="">${cam.label}</a>`
+    //   );
+    // });
   }
-  
-  
+
   async leave() {
     for (let trackName in this.localTracks) {
       var track = this.localTracks[trackName];
-      if(track) {
+      if (track) {
         track.stop();
         track.close();
         this.localTracks[trackName] = undefined;
       }
     }
-  
-    this.localTracks = { videoTrack: '',
-      audioTrack: ''
-    };
-  
+
+    this.localTracks = { videoTrack: '', audioTrack: '' };
+
     // remove remote users and player views
     this.remoteUsers = {};
-    $("#remote-playerlist").html("");
-  
+    $('#remote-playerlist').html('');
+
     // leave the channel
     await this.client.leave();
-  
-    $("#local-player-name").text("");
-    $("#join").attr("disabled", false);
-    $("#leave").attr("disabled", true);
-    $("#device-wrapper").css("display", "none");
-    console.log("client leaves channel success");
+
+    $('#local-player-name').text('');
+    $('#join').attr('disabled', false);
+    $('#leave').attr('disabled', true);
+    $('#device-wrapper').css('display', 'none');
+    console.log('client leaves channel success');
   }
-  
+
   async subscribe(user, mediaType) {
+    console.log('SUBSCRIBING');
     const uid = user.uid;
     // subscribe to a remote user
     await this.client.subscribe(user, mediaType);
-    console.log("subscribe success");
+    console.log('subscribe success');
     if (mediaType === 'video') {
       const player = $(`
         <div id="player-wrapper-${uid}">
@@ -244,48 +233,55 @@ $("#media-device-test").on("hidden.bs.modal", function (e) {
           <div id="player-${uid}" class="player"></div>
         </div>
       `);
-      $("#remote-playerlist").append(player);
+      $('#remote-playerlist').append(player);
       user.videoTrack.play(`player-${uid}`);
     }
     if (mediaType === 'audio') {
       user.audioTrack.play();
     }
   }
-  
-   handleUserPublished(user, mediaType) {
+
+  handleUserPublished(user, mediaType) {
+    console.log('USER HAS PUBLISHED !');
     const id = user.uid;
     this.remoteUsers[id] = user;
     this.subscribe(user, mediaType);
   }
-  
-   handleUserUnpublished(user, mediaType) {
+
+  handleUserUnpublished(user, mediaType) {
     if (mediaType === 'video') {
       const id = user.uid;
       delete this.remoteUsers[id];
       $(`#player-wrapper-${id}`).remove();
-  
     }
   }
-  
+
   async switchCamera(label) {
-    this.currentCam = this.cams.find(cam => cam.label === label);
-    $(".cam-input").val(this.currentCam.label);
+    console.log(label);
+    this.currentCam = this.cams.find((cam) => cam.label === label);
+    $('.cam-input').val(this.currentCam.label);
     // switch device of local video track.
     await this.localTracks.videoTrack.setDevice(this.currentCam.deviceId);
   }
-  
+
   async switchMicrophone(label) {
-    this.currentMic = this.mics.find(mic => mic.label === label);
-    $(".mic-input").val(this.currentMic.label);
+    console.log(label);
+    this.currentMic = this.mics.find((mic) => mic.label === label);
+    $('.mic-input').val(this.currentMic.label);
     // switch device of local audio track.
     await this.localTracks.audioTrack.setDevice(this.currentMic.deviceId);
   }
-  
-  // show real-time volume while adjusting device. 
+
+  // show real-time volume while adjusting device.
   setVolumeWave() {
     this.volumeAnimation = requestAnimationFrame(this.setVolumeWave);
-    $(".progress-bar").css("width", this.localTracks.audioTrack.getVolumeLevel() * 100 + "%")
-    $(".progress-bar").attr("aria-valuenow", this.localTracks.audioTrack.getVolumeLevel() * 100)
+    $('.progress-bar').css(
+      'width',
+      this.localTracks.audioTrack.getVolumeLevel() * 100 + '%'
+    );
+    $('.progress-bar').attr(
+      'aria-valuenow',
+      this.localTracks.audioTrack.getVolumeLevel() * 100
+    );
   }
- 
-  }
+}
