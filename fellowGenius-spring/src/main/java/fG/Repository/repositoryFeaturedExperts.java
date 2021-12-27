afec29b7036b0879749943d3226866ddc4b97be4
SuyashKejriwal <suyashkejriwal1308@gmail.com>
2021-12-27 20:55:34 +0530

    spring commit

--- fellowGenius-spring/src/main/java/fG/Repository/repositoryFeaturedExperts.java
+++
@@ -1,9 +0,0 @@
-package fG.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fG.Entity.FeaturedExperts;
-
-public interface repositoryFeaturedExperts extends JpaRepository<FeaturedExperts,Integer> {
-
-}
